f3eeae6b8ca4c67a5def3490f5489ba5b76673a1
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-01-19 21:00:00 +0900

    add host

---
+++ host/app/entry.client.tsx
@@ -0,0 +1,25 @@
+import { RemixBrowser } from '@remix-run/react'
+import { startTransition, StrictMode } from 'react'
+import { hydrateRoot } from 'react-dom/client'
+import { registerPiercingFragmentOutlet } from 'piercing-library'
+
+function hydrate() {
+  startTransition(() => {
+    hydrateRoot(
+      document,
+      <StrictMode>
+        <RemixBrowser />
+      </StrictMode>
+    )
+  })
+}
+
+registerPiercingFragmentOutlet()
+
+if (typeof requestIdleCallback === 'function') {
+  requestIdleCallback(hydrate)
+} else {
+  // Safari doesn't support requestIdleCallback
+  // https://caniuse.com/requestidlecallback
+  setTimeout(hydrate, 1)
+}
